d695f833df7749b378db3c40e56449364b22a6dd
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-03-23 23:11:26 +0530

    :lock: next auth config

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,61 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { compare } from "bcrypt";
+
+import prismadb from "@/lib/prismadb";
+
+export default NextAuth({
+  providers: [
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email or password required");
+        }
+
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.hasedPassword) {
+          throw new Error("Email does not exist");
+        }
+
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hasedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Password is incorrect");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
